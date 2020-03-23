b5c3473cb585faa68d571a3a1f8dcdc3eebeb5d2
shuxun_study <shuxunyer@163.com>
2020-03-23 21:38:28 +0800

    add addressConstant

---
+++ src/main/java/com/trueweb3j/common/AddressConstant.java
@@ -0,0 +1,22 @@
+package com.trueweb3j.common;
+
+public class AddressConstant {
+
+    public static String EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+    public static String fromPrivateKey = "";
+
+    public static String fromAddress = "0x73dbc7712E8578827ce84fB8926A8D6c4480205a";
+
+    public static String paymentPrivatekey = "";
+
+    public static String paymentAddress = "0x972770f317f0244fb722af1bab67454f2a43c20f";
+
+    public static String toAddress = "0x8926A8D6c4480205a73dbc7712E8578827ce84fB";
+
+    //er20代币合约地址
+    public static String contractAddress = "0x189cF5e2079686ff909EfdA350D5c66d28B6857a";
+
+    public static String contractAddress_map = "0x189cF5e2079686ff909EfdA350D5c66d28B6857a";
+
+}
