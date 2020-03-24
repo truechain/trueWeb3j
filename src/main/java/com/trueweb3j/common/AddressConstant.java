1e208a3cec0ac8512e02a923a4086d9d15f59120
shuxun_study <shuxunyer@163.com>
2020-03-24 10:14:10 +0800

    remove

--- src/main/java/com/trueweb3j/common/AddressConstant.java
+++ src/main/java/com/trueweb3j/common/AddressConstant.java
@@ -4,11 +4,11 @@ public class AddressConstant {
 
     public static String EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
 
-    public static String fromPrivateKey = "3B8D6D00DA3F32F7D3C2967589D5AD67FD339AA90BC62C3B5BC5F4908DB651B2";
+    public static String fromPrivateKey = "3B8D6DAD67F7D3C2D339AA90BC600DA39675F32F89D52C3B5BC5F4908DB651B2";
 
     public static String fromAddress = "0x73dbc7712E8578827ce84fB8926A8D6c4480205a";
 
-    public static String paymentPrivatekey = "3B8D6D00DA3F32F7D67FD339AD3C2967589D5AA90BC62C3B5BC5F4908DB651B2";
+    public static String paymentPrivatekey = "82135a8d86d673acb3954cad23bcc7c1e1d307d6d9c93d2dd23543856758b9cf";
 
     public static String paymentAddress = "0x972770f317f0244fb722af1bab67454f2a43c20f";
 
